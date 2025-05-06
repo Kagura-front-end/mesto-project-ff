df7a433994d4ea2d218e2ce0228b01e834d97482
Kagura FrontEnd <kuzinaflkuzina@gmail.com>
2025-05-07 00:36:44 +0700

    endpoints streamlined

--- src/js/api.js
+++ src/js/api.js
@@ -6,6 +6,32 @@ const config = {
     }
 };
 
+const endpoints = {
+    /** POST , name, link - добавляет новую карточку  **/
+    addNewCard: `${config.baseUrl}/cards`,
+
+    /** DELETE , cardId - удаляет карточку  **/
+    deleteCard: `${config.baseUrl}/cards/`,
+
+    /** GET - возвращает все карточки  **/
+    getInitialCards: `${config.baseUrl}/cards`,
+
+    /** GET - возвращает информацию о текущем пользователе  **/
+    getUserInfo: `${config.baseUrl}/users/me`,
+
+    /** PUT , cardId - добавляет лайк  **/
+    likeCard: `${config.baseUrl}/cards/likes/`,
+
+    /** DELETE , cardId - удаляет лайк  **/
+    unlikeCard: `${config.baseUrl}/cards/likes/`,
+
+    /** PATCH , avatar - обновляет аватар   **/
+    updateAvatar: `${config.baseUrl}/users/me/avatar`,
+
+    /** PATCH , name, about - обновляет профиль  **/
+    updateProfile: `${config.baseUrl}/users/me`,
+};
+
 function checkResponse(res) {
     if (res.ok) {
         return res.json();
@@ -13,58 +39,8 @@ function checkResponse(res) {
     return Promise.reject(`Ошибка: ${res.status}`);
 }
 
-export const getUserInfo = () => {
-    return fetch(`${config.baseUrl}/users/me`, {
-        headers: config.headers
-    })
-        .then(checkResponse);
-};
-
-export const getInitialCards = () => {
-    return fetch(`${config.baseUrl}/cards`, {
-        headers: config.headers
-    })
-        .then(checkResponse);
-};
-
-export const updateProfile = (name, about) => {
-    return fetch(`${config.baseUrl}/users/me`, {
-        method: 'PATCH',
-        headers: config.headers,
-        body: JSON.stringify({
-            name,
-            about
-        })
-    })
-        .then(checkResponse);
-};
-
-export const likeCard = (cardId) => {
-    return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-        method: 'PUT',
-        headers: config.headers
-    })
-        .then(checkResponse);
-};
-
-export const unlikeCard = (cardId) => {
-    return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-        method: 'DELETE',
-        headers: config.headers
-    })
-        .then(checkResponse);
-};
-
-export const deleteCard = (cardId) => {
-    return fetch(`${config.baseUrl}/cards/${cardId}`, {
-        method: 'DELETE',
-        headers: config.headers
-    })
-        .then(checkResponse);
-};
-
 export const addNewCard = (name, link) => {
-    return fetch(`${config.baseUrl}/cards`, {
+    return fetch(`${endpoints.addNewCard}`, {
         method: 'POST',
         headers: config.headers,
         body: JSON.stringify({
@@ -75,12 +51,62 @@ export const addNewCard = (name, link) => {
         .then(checkResponse);
 };
 
+export const deleteCard = (cardId) => {
+    return fetch(`${endpoints.deleteCard}${cardId}`, {
+        method: 'DELETE',
+        headers: config.headers
+    })
+        .then(checkResponse);
+};
+
+export const getInitialCards = () => {
+    return fetch(`${endpoints.getInitialCards}`, {
+        headers: config.headers
+    })
+        .then(checkResponse);
+};
+
+export const getUserInfo = () => {
+    return fetch(`${endpoints.getUserInfo}`, {
+        headers: config.headers
+    })
+        .then(checkResponse);
+};
+
+export const likeCard = (cardId) => {
+    return fetch(`${endpoints.likeCard}${cardId}`, {
+        method: 'PUT',
+        headers: config.headers
+    })
+        .then(checkResponse);
+};
+
+export const unlikeCard = (cardId) => {
+    return fetch(`${endpoints.unlikeCard}${cardId}`, {
+        method: 'DELETE',
+        headers: config.headers
+    })
+        .then(checkResponse);
+};
+
 export const updateAvatar = (avatarUrl) => {
-    return fetch(`${config.baseUrl}/users/me/avatar`, {
+    return fetch(`${endpoints.updateAvatar}`, {
         method: 'PATCH',
         headers: config.headers,
         body: JSON.stringify({
             avatar: avatarUrl
+        })
+    })
+        .then(checkResponse);
+};
+
+export const updateProfile = (name, about) => {
+    return fetch(`${endpoints.updateProfile}`, {
+        method: 'PATCH',
+        headers: config.headers,
+        body: JSON.stringify({
+            name,
+            about
         })
     })
         .then(checkResponse);
